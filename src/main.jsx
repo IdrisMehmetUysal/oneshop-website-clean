6daf1b6fadfc98c1073eb5299051e131d23b0c6c
Mehmet <150623296+IdrisMehmetUysal@users.noreply.github.com>
2026-04-02 15:51:41 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import App from "./App";
 import Impressum from "./Impressum";
 import Datenschutz from "./Datenschutz";
+import IphoneReparatur from "./IphoneReparatur";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -15,6 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           <Route path="/" element={<App />} />
           <Route path="/impressum" element={<Impressum />} />
           <Route path="/datenschutz" element={<Datenschutz />} />
+          <Route path="/iphone-reparatur-st-poelten" element={<IphoneReparatur />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
